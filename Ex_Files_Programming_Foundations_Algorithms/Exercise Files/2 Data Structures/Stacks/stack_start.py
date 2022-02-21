bd3dd1ae201bcdf71c0670550f62da1e1c868253
Sami Dollars <muss.junior@gmail.com>
2022-02-21 12:07:50 -0600

    new update

--- Ex_Files_Programming_Foundations_Algorithms/Exercise Files/2 Data Structures/Stacks/stack_start.py
+++ Ex_Files_Programming_Foundations_Algorithms/Exercise Files/2 Data Structures/Stacks/stack_start.py
@@ -1,12 +1,18 @@
 # try out the Python stack functions
 
 # TODO: create a new empty stack: new
-
+stack = []
 
 # TODO: push items onto the stack
-
+stack.append('S')
+stack.append('A')
+stack.append('M')
+stack.append('I')
 
 # TODO: print the stack contents
-
+print(stack)
 
 # TODO: pop an item off the stack
+x = stack.pop()
+print(x)
+print(stack)

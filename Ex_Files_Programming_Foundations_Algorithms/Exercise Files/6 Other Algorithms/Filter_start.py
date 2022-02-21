41bbe7cf0cb9951d5bad954f71f0202d539ab678
Sami Dollars <muss.junior@gmail.com>
2022-02-21 14:11:12 -0600

    done

--- Ex_Files_Programming_Foundations_Algorithms/Exercise Files/6 Other Algorithms/Filter_start.py
+++ Ex_Files_Programming_Foundations_Algorithms/Exercise Files/6 Other Algorithms/Filter_start.py
@@ -7,9 +7,12 @@ items = ["apple", "pear", "orange", "banana", "apple",
          "apple", "kiwi", "pear", "apple", "orange"]
 
 # TODO: create a hashtable to perform a filter
-
+filter = dict()
 
 # TODO: loop over each item and add to the hashtable
-
+for item in items:
+    filter[item] = 0
 
 # TODO: create a set from the resulting keys in the hashtable
+result = set(filter.keys())
+print(result)

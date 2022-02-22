0086baa98aff88e1fdb9e01faacf134f88f08f7d
MOUSSA  SANOU <58495791+moussa-sanou@users.noreply.github.com>
2022-02-22 09:53:08 -0600

    Add files via upload

---
+++ Ex_Files_Python_Essential_Libraries/Exercise Files/Pendulum/basicdates_start.py
@@ -0,0 +1,27 @@
+# Python Essential Libraries by Joe Marini course example
+# Example file for Pendulum library
+from datetime import datetime
+import time
+import pendulum
+
+# TODO: create a new datetime using pendulum
+dt1 = pendulum.datetime(2022, 2, 22, tz = "America/New_York")
+print(dt1)
+
+print(isinstance(dt1, datetime))
+
+print(dt1.timezone.name)
+
+# TODO: convert the time to another time zone
+
+
+# TODO: create a new datetime using the now() function
+
+
+# TODO: Use the local function function
+
+
+# TODO: Use today, tomorrow, yesterday
+
+
+# TODO: create a datetime from a system timestamp

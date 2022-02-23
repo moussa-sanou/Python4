44d7579f5fd30f6851edfaf1d365e6f906792685
Sami Dollars <muss.junior@gmail.com>
2022-02-23 11:39:30 -0600

    file exo completed

--- Ex_Files_Python_Essential_Libraries/Exercise Files/PyFilesystem/walking_start.py
+++ Ex_Files_Python_Essential_Libraries/Exercise Files/PyFilesystem/walking_start.py
@@ -7,13 +7,30 @@ from fs.zipfs import ZipFS
 # create a basic file walker
 with OSFS(".") as myfs:
     print("-- Files --")
+
     # TODO: use the files walker to process files
+    for path in myfs.walk.files(filter=["*.txt"]):
+        print(path)
 
     print("-- Directories --")
     # TODO: use the dirs walker for directories
+    for path in myfs.walk.dirs():
+        print(path)
 
 # TODO: use the info property to step through items
+with OSFS(".") as myfs:
+    for path, info in myfs.walk.info(namespaces=["details"]):
+        print(path, info.is_dir, info.size)
 
 # TODO: Use the walk object by itself:
+with OSFS("FileExamples") as myfs:
+    for step in myfs.walk():
+        print(step.path)
+        print(step.files)
+        print(step.dirs)
 
 # TODO: Use the walker with a ZIP
+with ZipFS("FileExamples.zip") as thezip:
+    print("-- zip Contents --")
+    for path in thezip.walk.files():
+        print(path)

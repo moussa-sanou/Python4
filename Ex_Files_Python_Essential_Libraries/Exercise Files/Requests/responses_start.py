543248f0671e47be1e9ba1bc5126f2b55189dfc1
Sami Dollars <muss.junior@gmail.com>
2022-02-23 09:38:41 -0600

    get response completed

--- Ex_Files_Python_Essential_Libraries/Exercise Files/Requests/responses_start.py
+++ Ex_Files_Python_Essential_Libraries/Exercise Files/Requests/responses_start.py
@@ -3,9 +3,18 @@
 import requests
 
 # TODO: work with status codes
-
+resp = requests.get("https://httpbin.org/status/200")
+print(resp.status_code)
+resp.raise_for_status()
 
 # TODO: examine response encoding
-
+resp = requests.get("https://httpbin.org/status/html")
+print(resp.encoding)
+print(resp.text)
+print(resp.content)
 
 # TODO: To read JSON content, use the json() function
+resp = requests.get("https://httpbin.org/json")
+print(resp.json())
+print(resp.headers)
+print(resp.headers['content-type'])

6953a010f3b19ce80fd47d0cd83f9baa0c6a8565
Sami Dollars <muss.junior@gmail.com>
2022-02-23 09:26:55 -0600

    get request completed

--- Ex_Files_Python_Essential_Libraries/Exercise Files/Requests/basicreqs_start.py
+++ Ex_Files_Python_Essential_Libraries/Exercise Files/Requests/basicreqs_start.py
@@ -3,12 +3,21 @@
 import requests
 
 # TODO: create a basic request for data
-
+resp = requests.get("https://httpin.org/xml")
+print(resp.status_code)
+print(resp.text)
 
 # TODO: create a request using parameters
-
+args = {"key1": 1, "key2": "two", "key3": False}
+resp = requests.get("https://httpin.org/get", params=args)
+print(resp.text)
+print(resp.url)
 
 # TODO: create a request using POST
-
+resp = requests.post("https://httpin.org/post", data={"key": "value"})
+print(resp.text)
 
 # TODO: create a request using custom headers
+heads = {"my-custom-header": "this is a custom header"}
+resp = requests.get("https://httpin.org/get", headers=heads)
+print(resp.text)

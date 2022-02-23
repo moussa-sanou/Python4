715ee9e24076ed9f2762a8f09a4106bc39ee41ca
Sami Dollars <muss.junior@gmail.com>
2022-02-23 10:08:34 -0600

    get adv completed

--- Ex_Files_Python_Essential_Libraries/Exercise Files/Requests/advreqs_start.py
+++ Ex_Files_Python_Essential_Libraries/Exercise Files/Requests/advreqs_start.py
@@ -3,9 +3,35 @@
 import requests
 
 # TODO: use a timeout value for a request
+# resp =  requests.get("https://httpbin.org/delay/2.5", timeout=1.0)
+# print(resp.status_code)
 
 
 # TODO: introspect the redirection history
-
+# resp = requests.get("http://github.com")
+# print(resp.url)
+# print(resp.history)
+# orig = resp.history.pop()
+# print(orig.status_code)
+# print(orig.url)
+# print(orig.reason)
 
 # TODO: Use a session object to group requests and settings
+sess =  requests.session()
+# sess.get("https://httpbin.org/cookies/set/sample/123456789")
+#
+# resp = sess.get("https://httpbin.org/cookies")
+# print(resp.text)
+
+sess.headers.update({
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) relesys_web_client/1.3.10.0 (RelesysApp/1.3.43 net.relesysapp.nettoenterprise)"
+})
+
+resp = sess.get("http://google.com")
+print(len(resp.content))
+
+sess.headers.update({
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0"
+})
+resp = sess.get("http://google.com")
+print(len(resp.content))

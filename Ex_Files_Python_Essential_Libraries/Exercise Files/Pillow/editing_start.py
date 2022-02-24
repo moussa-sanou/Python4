c0217f7a58ef07da10ab5f39b7bb3f0b5b81ca5d
Sami Dollars <muss.junior@gmail.com>
2022-02-24 09:57:26 -0600

    pillow exo completed

--- Ex_Files_Python_Essential_Libraries/Exercise Files/Pillow/editing_start.py
+++ Ex_Files_Python_Essential_Libraries/Exercise Files/Pillow/editing_start.py
@@ -4,6 +4,18 @@ from PIL import Image, ImageDraw, ImageFont
 
 infile = "ImagesArchive/Connections.jpeg"
 # TODO: use the ImageDraw routines to modify an image
-
+with Image.open(infile) as img:
+    draw =  ImageDraw.Draw(img)
+    draw.line((0,0) + img.size, width=40, fill=128)
+    draw.line((0, img.size[1], img.size[0],0), width=40, fill=128)
+    img.show()
 
 # TODO: use the ImageDraw routines to modify an image
+with Image.open(infile) as img:
+    draw =  ImageDraw.Draw(img)
+    textstr = "This is some text"
+    txtfont = ImageFont.truetype("Arial.ttf", 75)
+    txtsize = draw.textsize(textstr, font=txtfont)
+    location = (20, img.height - txtsize[1])
+    draw.text(location, textstr, (255, 255, 255), font=txtfont)
+    img.show()

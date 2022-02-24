4902d53ea3c9ed9c89e8cee705962ea1823b3656
Sami Dollars <muss.junior@gmail.com>
2022-02-24 10:45:24 -0600

    standard Library essential

---
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_02/basicstr_start.py
@@ -0,0 +1,16 @@
+# use predefined string constants
+import string
+
+
+# TODO: predefined string constants can be used in common scenarios
+print(string.ascii_letters)
+print(string.digits)
+print(string.hexdigits)
+print(string.punctuation)
+
+# TODO: Use the constants to filter information out
+test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of December"
+test_string2 = "Supercalifragilistic"
+test_string3 = "90210"
+
+# TODO: String testing methods

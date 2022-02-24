45f5fdd0e68d8b9199b5fa2b11c3b8c154e455b3
Sami Dollars <muss.junior@gmail.com>
2022-02-24 09:31:35 -0600

    pillow exo completed

--- Ex_Files_Python_Essential_Libraries/Exercise Files/Pillow/basics_start.py
+++ Ex_Files_Python_Essential_Libraries/Exercise Files/Pillow/basics_start.py
@@ -3,9 +3,21 @@
 from PIL import Image
 
 # TODO: read an image and examine some basic attributes using the Image class
-
+image = Image.open("ImagesArchive/Data.jpeg")
+print(image.filename)
+print(image.format)
+print(image.size)
+print(image.height)
+print(image.width)
+print(image.mode)
+for k,v in image.info.items():
+    print("key: {0}, value: {1}".format(k,v))
 
 # TODO: use the save function to convert an image to a new type
-
+outfile = "ImagesArchive/Data.png"
+image.save(outfile, "PNG")
+with Image.open(outfile) as im:
+    print("Image format: ", im.format)
 
 # TODO: show the image using the platform viewer app
+image.show()

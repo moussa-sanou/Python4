c373b1c102594e010b05ae49a2ddf3361269e98a
Sami Dollars <muss.junior@gmail.com>
2022-02-25 09:48:19 -0600

    standard Library essential

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_06/strformat_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_06/strformat_start.py
@@ -1,6 +1,6 @@
 # String formatting methods and best practices
 from string import Template
-
+import datetime
 
 # TODO: Using Template strings
 the_str = "The quick brown $animal $action over the lazy dog"
@@ -27,3 +27,6 @@ print("Output: {var2:x}, {var2:X}, {var1}".format(var1=foo,var2=bar))
 product = "Widget"
 price = 19.99
 tax = 0.07
+nyd = datetime.datetime(2019,1,1)
+print(f"{product} has a price of {price}, with tax{tax:.2%} the total is {round(price+(price*tax),2)}")
+print(f"But only on {nyd:%B %d %Y}")

a2cdca85ff110dd80ef8638780cbdf0f6e60344c
Sami Dollars <muss.junior@gmail.com>
2022-02-25 10:11:17 -0600

    standard Library essential

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 2/02_04/sortoperators_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 2/02_04/sortoperators_start.py
@@ -29,10 +29,15 @@ prodList = [
 # attrgetter() retrieves a given attribute or property from an object
 # itemgetter() retrieves an item at a given index in a collection
 # methodcaller() calls the given method on the object
-print("Using the attrgetter method:")
 
-# print("Using methodcaller to invoke a method:")
+print("Using the attrgetter method:")
+print(sorted(prodList, key=attrgetter("weight"), reverse=True))
+
+print("Using methodcaller to invoke a method:")
+print(sorted(prodList, key=methodcaller("discountPrice")))
 
 # TODO: Use itemgetter to retrieve an index
 inventory = [("Widget A", 5), ("Widget B", 2), ("Widget C", 4),
              ("Widget D", 7), ("Widget E", 4)]
+
+print(sorted(inventory, key=itemgetter(1)))

b4c1d815b34d0a061724d43f7ed6985487c6b1ca
Sami Dollars <muss.junior@gmail.com>
2022-02-25 09:21:32 -0600

    standard Library essential

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_03/search_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_03/search_start.py
@@ -4,14 +4,14 @@
 sampleStr = "The quick brown fox jumps over the lazy dog"
 
 # TODO: startsWith and endsWith functions
-# print(sampleStr.startswith("The"))
-# print(sampleStr.startswith("the"))
-# print(sampleStr.endswith("dog"))
+print(sampleStr.startswith("The"))
+print(sampleStr.startswith("the"))
+print(sampleStr.endswith("dog"))
 
 # TODO: the find and rfind functions
-# print(sampleStr.find("the"))
-# print(sampleStr.rfind("the"))
-# print("the" in sampleStr)
+print(sampleStr.find("the"))
+print(sampleStr.rfind("the"))
+print("the" in sampleStr)
 
 # TODO: using replace
 newStr = sampleStr.replace("lazy", "tired")

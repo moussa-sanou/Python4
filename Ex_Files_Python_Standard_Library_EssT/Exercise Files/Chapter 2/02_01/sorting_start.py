36f257ec4a3029b062622478ee1e3a7e94ace5c1
Sami Dollars <muss.junior@gmail.com>
2022-02-25 09:52:09 -0600

    standard Library essential

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 2/02_01/sorting_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 2/02_01/sorting_start.py
@@ -3,6 +3,9 @@
 testScores = [82, 67, 94, 73, 79, 86, 58, 91, 89, 71, 88, 77, 84]
 
 # TODO: perform a simple sort using sorted()
+sortedScores = sorted(testScores)
+print(sortedScores)
 
 # TODO: explicitly declare a sort order - ascending and descending
-
+sortedScores = sorted(testScores, reverse=True)
+print(sortedScores)

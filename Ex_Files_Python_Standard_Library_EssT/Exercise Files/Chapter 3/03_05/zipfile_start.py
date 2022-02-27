332e510cf5a63d39a977c4956cc6d6fde82e060e
Sami Dollars <muss.junior@gmail.com>
2022-02-27 10:14:27 -0600

    working with files

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 3/03_05/zipfile_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 3/03_05/zipfile_start.py
@@ -3,20 +3,27 @@ import zipfile
 
 
 # TODO: Create a new ZIP archive
-
+# zfile = zipfile.ZipFile("archive.zip", "w")
+# zfile.write("file1.txt")
+# zfile.write("file2.txt")
+# zfile.write("file3.txt")
 
 # TODO: Check validity of the file
-
+print(zipfile.is_zipfile("archive.zip"))
 
 # TODO: Read the properties of a ZIP archive
-
+zfile = zipfile.ZipFile("archive.zip")
+# print(zfile.namelist())
+# print(zfile.infolist())
 
 # TODO: Read the properties of ZIP contents
-
+zinfo = zfile.getinfo("file1.txt")
+# print(zinfo.file_size)
+# print(zfile.read("file3.txt"))
 
 # TODO: Extract ZIP file contents
 # zfile.extract("file2.txt")
-# zfile.extractall()
+zfile.extractall()
 
 
 # TODO: Ensure that the file is closed

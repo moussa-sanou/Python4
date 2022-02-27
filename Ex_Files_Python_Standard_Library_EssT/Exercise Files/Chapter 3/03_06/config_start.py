7b6488dbc51cb9a65c3b0a36816e861d03c50d1a
Sami Dollars <muss.junior@gmail.com>
2022-02-27 10:31:00 -0600

    working with files

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 3/03_06/config_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 3/03_06/config_start.py
@@ -9,19 +9,19 @@ parser = configparser.ConfigParser()
 parser.read("config.cfg")
 
 # TODO: print the sections
-# print(parser.sections())
-# print(parser.has_section("Section 1"))
+print(parser.sections())
+print(parser.has_section("Section 1"))
 
 # TODO: Access one of the default values
-# using_time_travel = bool(parser['DEFAULT']['UseTimeTravel'])
-# print(using_time_travel)
-# print(type(using_time_travel))
+using_time_travel = bool(parser['DEFAULT']['UseTimeTravel'])
+print(using_time_travel)
+print(type(using_time_travel))
 
 # TODO: Demonstrate the getXXX convenience functions
-# opd = parser['DEFAULT'].getboolean('ObeyPrimeDirective')
-# print(opd)
-# speed = parser['DEFAULT'].getfloat('Ship Speed')
-# print(speed)
+opd = parser['DEFAULT'].getboolean('ObeyPrimeDirective')
+print(opd)
+speed = parser['DEFAULT'].getfloat('Ship Speed')
+print(speed)
 
 # TODO: Access a non-existent value
 try:

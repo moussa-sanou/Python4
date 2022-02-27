eb4c86c3d0040c25f55962f5dd42779ccd2f4dc8
Sami Dollars <muss.junior@gmail.com>
2022-02-27 09:34:00 -0600

    working with files

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 3/03_02/tempfiles_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 3/03_02/tempfiles_start.py
@@ -3,12 +3,28 @@ import os
 import tempfile
 
 # TODO: get information about the temp data environment
-
+print('gettempir: ', tempfile.gettempdir())
+print('gettempprefix: ', tempfile.gettempprefix())
 
 # TODO: create a temporary file using mkstemp()
+(tempfh, tempfp) = tempfile.mkstemp(".tmp","testTemp", None,True)
+f = os.fdopen(tempfh, "w+t")
+f.write("This is some temp data")
+f.seek(0)
+print(f.read())
+f.close()
+os.remove(tempfp)
 
-
-# TODO: create a temp file using the TemporaryFile class
-
+#TODO: create a temp file using the TemporaryFile class
+with tempfile.TemporaryFile(mode="w+t") as tfp:
+    tfp.write("This is some temp data")
+    tfp.seek(0)
+    print(tfp.read())
 
 # TODO: create a temporary directory using the TemporaryDirectory class
+with tempfile.TemporaryDirectory() as tdp:
+    path = os.path.join(tdp, "tempfile.txt")
+    tfp = open(path,"w+t")
+    tfp.write("This is a temp file in a temp dir")
+    tfp.seek(0)
+    print(tfp.read())

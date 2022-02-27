2dd8693363a72f7b9fce1acf78637f39fb21bcbe
Sami Dollars <muss.junior@gmail.com>
2022-02-27 10:30:18 -0600

    working with files

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 3/03_06/config_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 3/03_06/config_start.py
@@ -3,19 +3,29 @@ import configparser
 
 
 # TODO: Create the configuration parser
-
+parser = configparser.ConfigParser()
 
 # TODO: Read the configuration file
-
+parser.read("config.cfg")
 
 # TODO: print the sections
-
+# print(parser.sections())
+# print(parser.has_section("Section 1"))
 
 # TODO: Access one of the default values
-
+# using_time_travel = bool(parser['DEFAULT']['UseTimeTravel'])
+# print(using_time_travel)
+# print(type(using_time_travel))
 
 # TODO: Demonstrate the getXXX convenience functions
-
+# opd = parser['DEFAULT'].getboolean('ObeyPrimeDirective')
+# print(opd)
+# speed = parser['DEFAULT'].getfloat('Ship Speed')
+# print(speed)
 
 # TODO: Access a non-existent value
-
+try:
+    title = parser['Section 3']['Title']
+    print(title)
+except KeyError as err:
+    print(err)

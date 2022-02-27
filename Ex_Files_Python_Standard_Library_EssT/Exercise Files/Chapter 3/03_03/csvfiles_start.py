af1496254ddad65295f95ed4a74089b4eb9ad62a
Sami Dollars <muss.junior@gmail.com>
2022-02-27 09:46:45 -0600

    working with files

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 3/03_03/csvfiles_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 3/03_03/csvfiles_start.py
@@ -3,16 +3,27 @@ import csv
 
 
 # TODO: list the dialects that are available to use
-
+print(csv.list_dialects())
 
 # TODO: Open a CSV file and read each row of data
 def readerSample():
-    pass
+    with open("StockQuotes.csv") as datafile:
+        reader = csv.reader(datafile)
+        for row in reader:
+            print(row)
 
 
 # TODO: Use the CSV module Sniffer to see what dialect of CSV this is
 def useSniffer():
-    pass
+    with open("StockQuotes.csv") as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        csvfile.seek(0)
+        hasHeader = csv.Sniffer().has_header(csvfile.read(1024))
+        csvfile.seek(0)
+        print("Headers found: " + str(hasHeader))
+        print(dialect.delimiter)
+        print(dialect.escapechar)
+        print(dialect.quotechar)
 
 
 # TODO: Write data to a CSV file
@@ -23,4 +34,4 @@ def writerSample():
 # Exercise the samples
 # readerSample()
 # writerSample()
-# useSniffer()
+useSniffer()

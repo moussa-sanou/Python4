665855cb40ab5e1ea6c2eeeef7ea8a70963117cc
Sami Dollars <muss.junior@gmail.com>
2022-02-27 09:18:07 -0600

    working with file

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 3/03_01/filesbasic_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 3/03_01/filesbasic_start.py
@@ -3,9 +3,18 @@
 
 # TODO: open a file for writing data
 # "w" means write, "r" means read, "a" means append, "r+" means read and write
-
+# fp = open("testfile.txt", "w")
+# fp.write("This is some text\n")
+# fp.close()
 
 # TODO: read a file's data
-
+# with open("testfile.txt", "r") as fp:
+#     data = fp.read()
+#     print(data)
 
 # TODO: Add data to an existing file
+with open("testfile.txt", "a+") as fp:
+    fp.write("This is data added onto the file\n")
+    fp.seek(0)
+    data = fp.read()
+    print(data)

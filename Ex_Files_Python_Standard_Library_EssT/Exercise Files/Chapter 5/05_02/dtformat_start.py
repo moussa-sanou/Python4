513797e2338b3843d07294880f2af5b76e1dd7b2
Sami Dollars <muss.junior@gmail.com>
2022-02-28 11:51:26 -0600

    working with datetime

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 5/05_02/dtformat_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 5/05_02/dtformat_start.py
@@ -5,22 +5,26 @@ import datetime
 now = datetime.datetime.now()
 
 # TODO: print various day and month formatting
-
-
-# TODO: print various time formatting
-
-
-# TODO: Locale-specific
+# print(now.strftime("%a, %A, %w, %d"))
+# print(now.strftime("%b, %B, %m"))
+#
+# # TODO: print various time formatting
+# print(now.strftime("%H, %I, %M, %S, %p"))
+#
+#
+# # TODO: Locale-specific
+# print(now.strftime("%c"))
+# print(now.strftime("%x"))
 
 
 # TODO: short date format (m/d/y)
-output = now.strftime("")
-# print("today is: ", output)
+output = now.strftime("%m/%d/%y")
+print("today is: ", output)
 
 # TODO: long date format (Day, number, Month, Year)
-output = now.strftime("")
-# print("today is: ", output)
+output = now.strftime("%A %d, %B %Y")
+print("today is: ", output)
 
 # TODO: short date and time (m/d/y, H:M am/pm)
-output = now.strftime("")
-# print("today is: ", output)
+output = now.strftime("%m/%d/%y %I:%M %p")
+print("today is: ", output)

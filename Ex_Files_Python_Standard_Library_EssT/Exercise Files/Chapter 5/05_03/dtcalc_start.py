ea5dd51780d189a40001e3b8a24a4891589e8911
Sami Dollars <muss.junior@gmail.com>
2022-02-28 11:58:42 -0600

    working with datetime

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 5/05_03/dtcalc_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 5/05_03/dtcalc_start.py
@@ -6,16 +6,22 @@ dt1 = datetime.datetime(2019, 1, 15, 10)
 dt2 = datetime.datetime(2019, 1, 20, 15)
 
 # TODO: dates and times can be compared
-
+print(dt1 < dt2)
+print(dt1 > dt2)
 
 # TODO: Subtracting one date from another creates a timedelta
+delta = dt2 - dt1
+print(delta)
 
 # timedeltas have components
-
+print(delta.days)
+print(delta.seconds)
 
 # TODO: timedeltas can be used to perform date math
 now = datetime.datetime.now()
+oneyear = datetime.timedelta(days=365)
+oneweek = datetime.timedelta(weeks=1)
 
-# print("One year from now will be: ")
-# print("One week from now will be: ")
-# print("One week ago was: ")
+print("One year from now will be: ", now + oneyear)
+print("One week from now will be: ", now + oneweek)
+print("One week ago was: ", now - oneweek)

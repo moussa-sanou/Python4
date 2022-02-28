5e321e9b54081a0700d78ba75c5b2c7c3d441613
Sami Dollars <muss.junior@gmail.com>
2022-02-28 11:26:03 -0600

    working with random

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 4/04_06/stats_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 4/04_06/stats_start.py
@@ -9,18 +9,18 @@ sample_data1 = [1, 3, 5, 7]
 sample_data2 = [2, 3, 5, 4, 3, 5, 3, 2, 5, 6, 4, 3]
 
 # TODO: Use the mean function - calculates an average value
-# print(statistics.mean(sample_data1))
+print(statistics.mean(sample_data1))
 
 
 # TODO: Use the different median functions
-# print(statistics.median(sample_data1))
-# print(statistics.median_low(sample_data1))
-# print(statistics.median_high(sample_data1))
+print(statistics.median(sample_data1))
+print(statistics.median_low(sample_data1))
+print(statistics.median_high(sample_data1))
 
 
 # TODO: The mode function indicates which data item occurs
 # most frequently
-# print(statistics.mode(sample_data2))
+print(statistics.mode(sample_data2))
 
 # Read data from a CSV file and calculate statistics
 def readData():

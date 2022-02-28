015fed9e9de89a11ba0f07859f09b0db55881741
Sami Dollars <muss.junior@gmail.com>
2022-02-28 10:12:20 -0600

    working with files

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 4/04_01/random_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 4/04_01/random_start.py
@@ -2,21 +2,33 @@
 import random
 
 # TODO: create a random number
-
+print(random.random())
+print(random.random())
+print(random.random())
 
 # TODO: implement a coin toss function
-
+for i in range(10):
+    if (random.random() <= 0.5):
+        print("Heads")
+    else:
+        print("Tails")
 
 # TODO: get a random number within a range
-
+print(random.uniform(1,100))
 
 # TODO: generate random integers within a given range
-
+print(random.randint(1,100))
 
 # TODO: generate random integers with a step function
 # this example chooses from 0 to 100 stepped by 5
-
+print(random.randrange(0,101,5))
 
 # TODO: Use the seed function to position the generator
+random.seed(1)
+print(random.random())
+print(random.random())
 
 print("----------------")
+random.seed(1)
+print(random.random())
+print(random.random())

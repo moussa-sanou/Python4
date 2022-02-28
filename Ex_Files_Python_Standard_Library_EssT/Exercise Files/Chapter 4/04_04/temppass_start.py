ee5de0f92a1f26622c2ac9431c55f858443735dd
Sami Dollars <muss.junior@gmail.com>
2022-02-28 10:45:35 -0600

    working with random

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 4/04_04/temppass_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 4/04_04/temppass_start.py
@@ -5,12 +5,21 @@ import string
 
 # TODO: Function to return a temporary password given a length
 def generateTempPass(numChars=8):
-    pass
+    potentialChars = string.ascii_letters + string.digits + "+=?/!@#$%*"
+    result = ''.join(secrets.choice(potentialChars) for i in range(numChars))
+    return result
 
 
 # TODO: Function to return a temporary password and enforce 1 number and 1 uppercase
 def generateBetterPass(numChars=8):
-    pass
+    potentialChars = string.ascii_letters + string.digits + "+=?/!@#$%*"
+    while True:
+        result = ''.join(secrets.choice(potentialChars) for i in range(numChars))
+
+        if(any(c.isupper() for c in result) and any(c.isdigit() for c in result)):
+            break
+    return result
+
 
 # create a temporary password
 # print(generateTempPass(10))
@@ -21,3 +30,5 @@ def generateBetterPass(numChars=8):
 
 # TODO: create a temporary, hard-to-guess URL
 resultUrl = "https://my.example.com?reset="
+resultUrl += secrets.token_urlsafe(15)
+print(resultUrl)

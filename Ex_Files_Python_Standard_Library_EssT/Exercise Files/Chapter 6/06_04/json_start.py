be2d24e0cc3921e4078a8e864ecc364185b6be28
Sami Dollars <muss.junior@gmail.com>
2022-03-01 10:01:35 -0600

    working with url

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 6/06_04/json_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 6/06_04/json_start.py
@@ -4,13 +4,17 @@ import json
 
 
 # TODO: use urllib to retrieve some sample JSON data
-
+req = urllib.request.urlopen("http://httpbin.org/json")
+data = req.read().decode('utf-8')
+# print(data)
 
 # TODO: use the JSON module to parse the returned data
-
+obj = json.loads(data)
 
 # TODO: when the data is parsed, we can access it like any other object
-
+# print(obj["slideshow"]["author"])
+# for slide in obj["slideshow"]["slides"]:
+#     print(slide["title"])
 
 # TODO: python objects can also be written out as JSON
 objdata = {
@@ -20,3 +24,5 @@ objdata = {
         "Learning Python", "Advanced Python", "Python Standard Library Essential Training"
     ]
 }
+with open("jsonoutput,json", "w") as fp:
+    json.dump(objdata, fp, indent=4)

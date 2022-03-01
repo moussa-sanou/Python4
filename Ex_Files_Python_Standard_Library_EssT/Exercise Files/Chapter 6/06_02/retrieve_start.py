d69a803356f587bbe67431d5ea770734b9039235
Sami Dollars <muss.junior@gmail.com>
2022-03-01 09:37:20 -0600

    working with url

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 6/06_02/retrieve_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 6/06_02/retrieve_start.py
@@ -5,16 +5,19 @@ import urllib.request
 sample_url = "http://httpbin.org/xml"
 
 # TODO: Create a request to retrieve data using urllib.request
-
+resp = urllib.request.urlopen(sample_url)
 
 # TODO: Check the status
-status_code = 0
-
+status_code = resp.status
+print(status_code)
 
 # TODO: if no error, then read the response contents
 if status_code >= 200 and status_code < 300:
     # TODO: work with response headers
-    pass
+    # print(resp.getheaders())
+    # print(resp.getheader("Content-lenght"))
+    # print(resp.headers["Content-Type"])
 
     # TODO: read the data from the URL
-    pass
+    data = resp.read().decode('utf-8')
+    print(data)

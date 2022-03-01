2eae722f555a46e81758cf9808cfb1ba47f4a92a
Sami Dollars <muss.junior@gmail.com>
2022-03-01 10:31:19 -0600

    working with url

--- Ex_Files_Learning_Python_3_Standard_Library_2022/Exercise Files/Ch01/01_01/01_01_Start.py
+++ Ex_Files_Learning_Python_3_Standard_Library_2022/Exercise Files/Ch01/01_01/01_01_Start.py
@@ -1,21 +1,34 @@
 # Python Logical Operators: And, Or, Not:
 
 # What is a Boolean?
-
+isRaining = True
+isSunny = True
 # Logical Operators -> Special Operators for Booleans
-#nmm
+
 # AND
 # true and true --> true
 # false and true --> false
 # true and false --> false
 # false and false --> false
-
+if isRaining and isSunny:
+    print("We might see a rainbow")
 # OR
 # true and true --> true
 # false and true --> true
 # true and false --> true
 # false and false --> false
 
+if isSunny or isRaining:
+    print("It might be rainy or sunny")
+
 # NOT
 # true --> false
 # false --> true
+if not isRaining:
+    print("It must be raining")
+
+ages = [12, 18, 39, 87, 7, 2]
+for age in ages:
+    isAdult = age > 17;
+    if not isAdult:
+        print("Being " + str(age) + "does not make you an adult. ")

2c44f45c6b9d0ddbf6b14907e625f3991e3d54ec
Sami Dollars <muss.junior@gmail.com>
2022-03-02 23:30:13 -0600

    Primitive data types

--- EPI/Primitive/count_bits.py
+++ EPI/Primitive/count_bits.py
@@ -1,1 +1,8 @@
-# Write a program
+# Write a program to count number of bits that are set to 1 in a nonnegative integer
+
+def count_bits(x):
+    num_bits = 0
+    while x:
+        num_bits += x & 1
+        x >>= 1
+    return num_bits

ed030b5bed2bc456d286e4b6148f5d1a692e24fc
Sami Dollars <muss.junior@gmail.com>
2022-03-10 09:11:22 -0600

    Array data types

---
+++ EPI/Arrays/duplicateZero.py
@@ -0,0 +1,30 @@
+
+'''Duplicate Zeros'''
+
+# Given a fixed-lenght integer array arr, duplicate each concurrence of zero, shiffting the remaining elements to the right
+# Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything
+
+# Example 1
+# Input: arr = [1,0,2,3,0,4,5,0]
+# Output: [1,0,0,2,3,0,0,4]
+# Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+# Example 2
+# Input: arr = [1,2,3]
+# Output: [1,2,3]
+# Explanation: After calling your function, the input array is modified to: [1,2,3]
+
+class Solution(object):
+    def duplicateZeros(self, arr):
+        i = 0
+        while i < len(arr)-1:
+            if arr[i]== 0:
+                arr.insert(i+1,0)
+                arr.pop()
+                i+=1
+            i+=1
+
+
+
+
+

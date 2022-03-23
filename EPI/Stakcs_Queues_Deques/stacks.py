f95826401df6dbe9746f384456c5aaca6b829464
Sami Dollars <muss.junior@gmail.com>
2022-03-23 10:15:24 -0500

    stack application

--- EPI/Stakcs_Queues_Deques/stacks.py
+++ EPI/Stakcs_Queues_Deques/stacks.py
@@ -1,31 +1,14 @@
-
-
-class Stack(object):
-    def __int__(self):
+class stack:
+    def __init__(self):
         self.items = []
 
     def push(self, item):
-        '''Acceepts an item as a parameter and appends it to the end of the list. Returns nothing
-        The runtime for this method is O(1), or constant time, because appending
-        to the end of a list happens constant time'''
+        '''Accepts an item as a paramater and appends it to the end of the list. Returns nothing.
 
+        The runtime for this method is o(1), or constant time, because appending to end of a list happens in constant time.'''
         self.items.append(item)
 
-    def pop(self):
-        pass
-
-    def peek(self):
-        pass
-
-    def size(self):
-        pass
-
-    def is_empty(self):
-        pass
-
-if __name__=="__main__":
-
-    my_stack = Stack()
-    my_stack.push('apple')
-    print( my_stack.push('apple'))
-
+my_stack = stack()
+my_stack.push('apple')
+my_stack.items
+print(my_stack.items)

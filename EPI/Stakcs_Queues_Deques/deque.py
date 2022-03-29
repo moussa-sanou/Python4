94904e310612423024389846abdbd93e4dad75a4
Sami Dollars <muss.junior@gmail.com>
2022-03-29 11:20:47 -0500

    queue implementation

---
+++ EPI/Stakcs_Queues_Deques/deque.py
@@ -0,0 +1,16 @@
+class Deque:
+
+    def __init__(self):
+        self.items = []
+
+    def add_front(self, item):
+        pass
+
+    def add_rear(self, item):
+        pass
+
+    def remove_front(self):
+        pass
+
+    def remove_rear(self):
+        pass

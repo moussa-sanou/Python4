5f8b9d25367bed8405af94b95d8464c5908ed065
Sami Dollars <muss.junior@gmail.com>
2022-03-29 11:23:03 -0500

    dequeue implementation

--- EPI/Stakcs_Queues_Deques/deque.py
+++ EPI/Stakcs_Queues_Deques/deque.py
@@ -14,3 +14,15 @@ class Deque:
 
     def remove_rear(self):
         pass
+
+    def peek_front(self):
+        pass
+
+    def peek_rear(self):
+        pass
+
+    def size(self):
+        pass
+
+    def is_empty(self):
+        pass

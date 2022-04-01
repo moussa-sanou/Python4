80f3a0e60767e940b8bc3e93b44fa2da99f4aaea
Sami Dollars <muss.junior@gmail.com>
2022-04-01 09:26:02 -0500

    Dicitionaries python

---
+++ EPI/Dictionaries/diction.py
@@ -0,0 +1,10 @@
+
+
+sal_info = dict()
+sal_info = {'Austin':911985, 'Dallas': 89999, 'San Jose': 100989, 'Atlanta': 89286}
+print(sal_info)
+
+if ('Dallas' in sal_info):
+    print(sal_info['Dallas'])
+else:
+    print("Not found")

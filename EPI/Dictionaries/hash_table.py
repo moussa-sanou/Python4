0979c2c8e70fff14918aef3a241256424536b053
Sami Dollars <muss.junior@gmail.com>
2022-04-04 09:28:50 -0500

    Dicitionaries python

---
+++ EPI/Dictionaries/hash_table.py
@@ -0,0 +1,3 @@
+
+
+# A hash table goal is to keep the data evenly distributed across the table, at least one-third empty
---
+++ EPI/Dictionaries/new_dic.py
@@ -0,0 +1,8 @@
+
+fif_team = {'Abidja': 'Asec Mimosa',
+            'Bouake': 'Bouake FC',
+            'Seguela':'AS Denguele',
+            'San Pedro': 'Wack FC',
+            'Bingerville': 'ES Bingerville'}
+
+print(fif_team)

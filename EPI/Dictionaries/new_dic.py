c54e0700bf6713be6e55dda07f6bfedb25e25767
Sami Dollars <muss.junior@gmail.com>
2022-04-04 09:41:59 -0500

    Dicitionaries python

--- EPI/Dictionaries/new_dic.py
+++ EPI/Dictionaries/new_dic.py
@@ -1,8 +1,20 @@
 
-fif_team = {'Abidja': 'Asec Mimosa',
+# Classic dictionary creation
+fif_team = {'Abidjan': 'Asec Mimosa',
             'Bouake': 'Bouake FC',
             'Seguela':'AS Denguele',
             'San Pedro': 'Wack FC',
             'Bingerville': 'ES Bingerville'}
 
 print(fif_team)
+
+# Create a dictionary using built in function
+fif_team2 = dict([('Abidjan', 'Asec Mimosa'),
+                  ('Bouake', 'BFC'),
+                  ('Seguela','AS Denguele'),
+                  ('San Pedro','Wack FC')])
+print(fif_team2)
+
+# Accessing Dictionaries values
+a = fif_team['Abidjan']
+print(a)

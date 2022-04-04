32b15da26186fddf124235c4c9af16b578e4903c
Sami Dollars <muss.junior@gmail.com>
2022-04-04 10:23:02 -0500

    Dicitionaries python

--- EPI/Dictionaries/new_dic.py
+++ EPI/Dictionaries/new_dic.py
@@ -18,3 +18,25 @@ print(fif_team2)
 # Accessing Dictionaries values
 a = fif_team['Abidjan']
 print(a)
+
+# Adding entry to an existing dictionary
+fif_team['korhogo'] = 'CO Korhogo'
+print(fif_team)
+
+# Update an entry
+fif_team['Bouake'] = 'Pakano Fc'
+print(fif_team)
+
+# Delete an entry
+del fif_team['Bingerville']
+print(fif_team)
+
+# Building a Dictionary Incrementally
+check_list = {}
+
+check_list['fname'] = 'Zoumanan'
+check_list['lname'] = 'Tokos'
+check_list['Age'] = '55'
+check_list['Spouse'] = 'Adrienne'
+check_list['Children'] = ['Paul', 'Pierre', 'Dan']
+print(check_list)

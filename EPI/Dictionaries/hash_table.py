e087a110bad5f1f6a7cd8a9e23d8d4fc321c1387
Sami Dollars <muss.junior@gmail.com>
2022-04-06 12:26:08 -0500

    Dicitionaries python

--- EPI/Dictionaries/hash_table.py
+++ EPI/Dictionaries/hash_table.py
@@ -4,3 +4,23 @@
 # A hash table amounts to a combination of two things with which we're quite familiar
 '''First, a hash function, which returns a nonnegative integer value called a hash code.
 Second,an array capable of storing data of type we wish to place into the data structure.'''
+
+print(hash(3.14))
+print(hash(3.14159265358979323846264338327950288419716939937510))
+print(hash("Lorem"))
+print(hash("Lorem ipsum dolor sit amet, consectetur adipisicing elit,"
+ "sed do eiusmod tempor incididunt ut labore et dolore magna"
+"aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
+"ullamco laboris nisi ut aliquip ex ea commodo consequat."
+"Duis aute irure dolor in reprehenderit in voluptate velit"
+"esse cillum dolore eu fugiat nulla pariatur. Excepteur sint"
+"occaecat cupidatat non proident, sunt in culpa qui officia"
+"deserunt mollit anim id est laborum."))
+
+print(hash(None))
+print(hash(hash))
+
+class hashtest:
+    pass
+
+print(hash(hashtest))

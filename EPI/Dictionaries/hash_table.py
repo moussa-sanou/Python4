c28e9092d0c1ed7d7eda09e287f404c0846ec3c8
Sami Dollars <muss.junior@gmail.com>
2022-04-05 20:07:29 -0500

    Dicitionaries python

--- EPI/Dictionaries/hash_table.py
+++ EPI/Dictionaries/hash_table.py
@@ -1,3 +1,6 @@
 
 
 # A hash table goal is to keep the data evenly distributed across the table, at least one-third empty
+# A hash table amounts to a combination of two things with which we're quite familiar
+'''First, a hash function, which returns a nonnegative integer value called a hash code.
+Second,an array capable of storing data of type we wish to place into the data structure.'''

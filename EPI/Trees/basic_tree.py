1e405c0680fe8ad0b27b80d761cc2a0aef74fc1b
Sami Dollars <muss.junior@gmail.com>
2022-04-15 09:36:51 -0500

    Tree implementation

--- EPI/Trees/basic_tree.py
+++ EPI/Trees/basic_tree.py
@@ -24,4 +24,8 @@ node.left.right = Node(6)
 node.right.left = Node(13)
 node.right.right = Node(10000)
 
+myTree = Tree(node, 'Sami\'s Tree')
 
+print(myTree.name)
+print(myTree.root.left.data)
+print(myTree.root.right.right.data)

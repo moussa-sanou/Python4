3206d357b80deee7d87f50a9dc92544fac7bc2db
Sami Dollars <muss.junior@gmail.com>
2022-05-04 11:14:04 -0500

    Graph Implementation

--- EPI/Graph/graph.py
+++ EPI/Graph/graph.py
@@ -10,4 +10,28 @@ graph = {
     "e":["d"]
 }
 
-print(graph)
+# print(graph)
+
+# Display graph vertices
+'''To display the graph vertices we simply find the keys of graph dictionary. We use the key() method'''
+
+class graph:
+    def __init__(self, gdict=None):
+        if gdict is None:
+            gdict = []
+        self.gdict = gdict
+    # Get the keys in the dictionary representing the vertices
+    def getVertices(self):
+        return list(self.gdict.keys())
+
+# Create a dictionary with graph element
+graph_elements = {
+    "a": ["b","c"],
+    "b": ["a","d"],
+    "c": ["a","d"],
+    "d": ["e"],
+    "e":["d"]
+}
+
+g = graph(graph_elements)
+print(g.getVertices())

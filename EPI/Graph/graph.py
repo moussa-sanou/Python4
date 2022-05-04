71244628bafb61a01717638e6030dd17fc099059
Sami Dollars <muss.junior@gmail.com>
2022-05-04 11:27:21 -0500

    Graph Implementation

--- EPI/Graph/graph.py
+++ EPI/Graph/graph.py
@@ -2,13 +2,13 @@
 
 # A graph can be represented using a dictionary
 
-graph = {
-    "a": ["b","c"],
-    "b": ["a","d"],
-    "c": ["a","d"],
-    "d": ["e"],
-    "e":["d"]
-}
+# graph = {
+#     "a": ["b","c"],
+#     "b": ["a","d"],
+#     "c": ["a","d"],
+#     "d": ["e"],
+#     "e":["d"]
+# }
 
 # print(graph)
 

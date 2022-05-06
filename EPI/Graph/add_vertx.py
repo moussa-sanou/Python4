f745c27d44aa297cd3dbaa0b7bc6eddb89a714de
Sami Dollars <muss.junior@gmail.com>
2022-05-05 20:47:16 -0500

    Adding a vertex to a graph

---
+++ EPI/Graph/add_vertx.py
@@ -0,0 +1,34 @@
+# Adding a vertex
+'''Adding a vertex is straight forward where we add another additional key to the graph dictionary'''
+
+
+class graph:
+    def __init__(self, gdict=None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+
+    def getvertices(self):
+        return list(self.gdict.keys())
+
+    # Add the vertex as a key
+    def addVertex(self, vrtx):
+        if vrtx not in self.gdict:
+            self.gdict[vrtx] = []
+
+
+# Create the dictionary with graph elements
+
+graph_elements = {
+    "a": ["b", "c"],
+    "b": ["a", "d"],
+    "c": ["a", "d"],
+    "d": ["e"],
+    "e": ["d"]
+}
+
+g = graph(graph_elements)
+g.addVertex("f")
+print(g.getvertices())
+
+

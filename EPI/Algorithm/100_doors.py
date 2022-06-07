4806a10af9887f1ddef04f144e949abb4dab8eec
Sami Dollars <muss.junior@gmail.com>
2022-06-06 19:48:34 -0500

    Algorithm

--- EPI/Algorithm/100_doors.py
+++ EPI/Algorithm/100_doors.py
@@ -1,12 +1,22 @@
 
 
-doors = [False] * 101 # False represent a closed door and True for a open door we use 101 so that we can ignore the door at index 0
-print(doors)
+# doors = [False] * 101 # False represent a closed door and True for a open door we use 101 so that we can ignore the door at index 0
+# print(doors)
+
+# for i in range(1, 101):
+#     doors[i] = not doors[i]
+# print(doors)
+#
+# for i in range(1, 6):
+#     for j in range(1, 4):
+#         print("i:", i, "j:", j)
+
+doors = [False] * 101 # So we can start at door no. 1 we will ignore index 0.
 
 for i in range(1, 101):
-    doors[i] = not doors[i]
-print(doors)
+    for j in range(i, 101, i):
+        doors[j] = not doors[j]
 
-for i in range(1, 6):
-    for j in range(1, 4):
-        print("i:", i, "j:", j)
+for i in range(1, 101):
+    if doors[i] is True:
+        print(i, end=", ")

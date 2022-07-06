7e8b08e7102b135f8e97ad16cf971c97a8956e50
Sami Dollars <muss.junior@gmail.com>
2022-07-05 22:09:55 -0500

    array leetcode

--- EPI/Arrays/leetcode/MrgStrArr.py
+++ EPI/Arrays/leetcode/MrgStrArr.py
@@ -19,19 +19,37 @@ last n elements set to 0 and should be ignored. nums has a lenght of n'''
 # Input: nums1 = [0], m = 0, nums2 = [1], n = 1
 # Output: [1]
 
-def merge(nums1, nums2):
-    for i in nums1 and nums2:
-        if i == 0:
-            return nums1.remove(i)
-        else:
-            nums3 = nums1 + nums2
-            solu = sorted(nums3)
-            return solu
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
 
+        # Make a copy of the first m elements of nums1
+        nums1_copy = nums1[:m]
+
+        # Read pointers for nums1copy and nums2
+        p1 = 0
+        p2 = 0
+
+        # Compare elements from nums1copy and nums2 and write the smallest to nums1.
+
+        for p in range(n + m):
+            # We also need to ensure that p1 and p2 aren't over the boundaries of their respective arrays.
+            if p2 >= n or (p1 < m and nums1_copy[p1] <= nums2[p2]):
+                nums1[p] = nums1_copy[p1]
+                p1 += 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 += 1
 
 
 
 nums1 = [1,2,3,0,0,0]
 nums2 = [2,5,6]
-res = merge(nums1, nums2)
-print(res)
+m = 3
+n = 3
+
+
+res = Solution()
+res.merge(nums1, m, nums2, n)
+print(res.merge(nums1, m, nums2, n))
+
+

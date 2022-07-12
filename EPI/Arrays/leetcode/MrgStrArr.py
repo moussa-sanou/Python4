1f2239aa98953f8c069b28c766f7e909affa631e
Sami Dollars <muss.junior@gmail.com>
2022-07-11 19:44:28 -0500

    array leetcode

--- EPI/Arrays/leetcode/MrgStrArr.py
+++ EPI/Arrays/leetcode/MrgStrArr.py
@@ -52,4 +52,21 @@ res = Solution()
 res.merge(nums1, m, nums2, n)
 print(res.merge(nums1, m, nums2, n))
 
+#
+# class Solution(object):
+#     def search(self, nums, target):
+#         left = 0
+#         right = len(nums) - 1
+#
+#         while left <= right:
+#             mid = (left + right) // 2
+#             if nums[mid] == target:
+#                 return mid
+#             elif target < nums[mid]:
+#                 right = mid - 1
+#             else:
+#                 left = mid + 1
+#         return -1
+
+
 

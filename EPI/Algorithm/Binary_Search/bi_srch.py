7a6a19ebf6cb60f05558ecfab3238a69ca98a8c7
Sami Dollars <muss.junior@gmail.com>
2022-07-26 21:26:46 -0500

    array leetcode

--- EPI/Algorithm/Binary_Search/bi_srch.py
+++ EPI/Algorithm/Binary_Search/bi_srch.py
@@ -29,3 +29,16 @@ class Solution(object):
         return -1
 
 
+class Solution(object):
+    def search(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            if target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 2
+        return -1

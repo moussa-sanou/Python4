aaba1fdcac51d09de61e6c086476b1f57f85da7f
Sami Dollars <muss.junior@gmail.com>
2023-01-06 23:54:23 -0600

    array leetcode

--- EPI/Algorithm/Merge_Sort/linked_list_merge.py
+++ EPI/Algorithm/Merge_Sort/linked_list_merge.py
@@ -26,4 +26,71 @@ def split(linked_list):
         return left_half, right_half
     else:
         size = linked_list.size()
+        mid = size // 2
+
+        mid_node = linked_list.node_at_index(mid - 1)
+
+        left_half = linked_list
+        right_half = linked_list()
+        right_half.head = mid_node.next_node
+        mid_node.next_node = None
+
+        return left_half, right_half
+
+def merge(left, right):
+    '''Merges two linked lists, sorting by data in nodes
+    Returns a new, merged list '''
+
+    # Create a new linked list that contains node from
+    # Merging left and right
+    merged = linkedlist()
+
+    # Add a fake head that is discarded later
+    merged.add(0)
+
+    # Set current to the head of the linked list
+    current = merged.head
+
+    # Obtain head nodes for left and right linked lists
+    left_head = left.head
+    right_head = right.head
+
+    # Iterate over left and right until we reach the tail node of either
+    while left_head or right_head:
+        # if the head node of left is None, we are past the tail
+        # Add the node from right to merged linked list
+        if left_head is None:
+            current.next_node = right_head
+            # Call next on right to set loop condition to False
+            right_head = right_head.next_node
+
+        # If the head node of right is None, we're past the tail
+        # Add the tail node from left to merged linked list
+        elif right_head is None:
+            current.next_node = left_head
+            # Call next on left to set loop condition to False
+            left_head = left_head.next_node
+        else:
+            # Not at either tail node
+            # Obtain node data to perform comparison operations
+            left_data = left_head.data
+            right_data = right_head.data
+            # If data on left is less than right, set current to left node
+            if left_head < right_data:
+                current.next_node = left_head
+                # Move left head to next node
+                left_head = left_head.next_node
+            # If data on left is greater than right, set current to right node
+            else:
+                current.next_node = right_head
+                # Move right head to next node
+                right_head = right_head.next_node
+        # Move current to next node
+        current = current.next_node
+
+    # Discard fake head and set first merged node as head
+    head = merged.head.next_node
+    merged.head = head
+
+    return merged
 
